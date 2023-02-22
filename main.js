c93d6a717debd1e393e92aae22bfa1427c8ba9a8
Marina <marina.zilkina@gmail.com>
2023-02-22 19:52:10 +0200

    Started a new quiz-project; html is ready; Css is ready;
    Quiclky install the webpack? :D

---
+++ main.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        question: "Какой язык работает в браузере?",
+        answers: ["Java", "C", "Python", "Javascript"],
+        correct: 4,
+    },
+    {
+        question: "Что означает CSS?",
+        answers: [
+            "Central Style Sheets",
+            "Cascading Style Sheets",
+            "Cascading Simple Sheets",
+            "Cars SUVs Sailboats",
+        ],
+        correct: 2,
+    },
+    {
+        question: "Что означает HTML",
+        answers: [
+            "Hypertext Markup Language",
+            "Hypertext Markdown Language",
+            "Hyperloop Machine Language",
+            "Helicopters Terminals Motoboats Lamborginis",
+        ],
+        correct: 1,
+    },
+    {
+        question: "В каком году был создан Javascript?",
+        answers: ["1996", "1995", "1994", "Все ответы неверные"],
+        correct: 2,
+    },
+];
